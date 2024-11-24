2bdb91c1c3c3bdf9f80db0986976f9b588bec332
Md.Sazzad Hossain Shaon <76906808+SazzadShaon25@users.noreply.github.com>
2024-11-24 09:04:06 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function main() {
     try{
         await mongoose.connect('mongodb+srv://stationary-shop-admin:3jTUFE8TswPC5qu2@cluster0.richl.mongodb.net/stationary-shop?retryWrites=true&w=majority&appName=Cluster0');
         app.listen(port, ()=>{
-            console.log(`listening on port ${port}`)
+            console.log(`Example app listening on port ${port}`)
         })
     }catch(error)
     {
